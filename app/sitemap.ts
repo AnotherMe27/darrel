7e8d850171865275914955736342f3963c6aa805
AnotherMe27 <AnotherMe27@users.noreply.github.com>
2026-08-27 22:04:03 +0300

    Make sitemap static for Pages export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,7 @@
 import type { MetadataRoute } from 'next';
 
+export const dynamic = 'force-static';
+
 const sitemap = (): MetadataRoute.Sitemap => {
   const baseUrl = 'https://www.soniaganda.com';
   return [
